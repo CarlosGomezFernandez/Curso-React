68ac25efb2fbb7a675c1d5011be4a49ae946d792
CarlosGomezFernandez <carlos.gomez33@alu.uclm.es>
2023-12-04 15:07:46 +0100

    Primeros componentes

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { TwitterFollowCard } from './TwitterFollowCard';
+
+const users = [
+    { userName: 'midudev', name: 'Miguel Ángel Durán', isFollowig: true },
+    { userName: 'facebook', name: 'Facebook', isFollowig: false },
+    { userName: 'WhatsApp', name: 'WhatsApp', isFollowig: false },
+];
+
+export function App () {
+
+    return (
+        <section className='App'>
+            {
+                
+            }
+        </section>
+    );
+}
